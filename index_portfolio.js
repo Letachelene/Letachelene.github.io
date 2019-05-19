060284850e673e603b46c140eae274500ddc3136
Helene Letac <helene.letac@coding-academy.fr>
2019-05-19 20:19:13 +0200

    portfolio layout avancee

--- index_portfolio.js
+++ index_portfolio.js
@@ -1,3 +1,64 @@
 function RedirectionJavascript() {
   document.location.href="CVDEV_copie.pdf";
 }
+
+function myFunction1() {
+  var elmnt = document.getElementById("digital");
+  elmnt.scrollIntoView();
+}
+
+function myFunction2() {
+  var elmnt = document.getElementById("projets");
+  elmnt.scrollIntoView();
+}
+
+function myFunction3() {
+  var elmnt = document.getElementById("formations");
+  elmnt.scrollIntoView();
+}
+
+function myFunction4() {
+  var elmnt = document.getElementById("experiences");
+  elmnt.scrollIntoView();
+}
+
+function myFunction5() {
+  var elmnt = document.getElementById("hobbies");
+  elmnt.scrollIntoView();
+}
+
+function myFunction6() {
+  var elmnt = document.getElementById("formulaire");
+  elmnt.scrollIntoView();
+}
+
+/*function animation_logos(element, numero){
+
+  var element = document.getElementsByClassName(element)[numero]
+  TweenMax.to(element, 1, {x:40});
+
+  var tl = new TimelineLite();
+  tl.to("logo_techno", 0.5, {rotation:360});
+
+  var techno = document.getElementsByClassName("logo_techno")
+  tl.to("logo_techno", 0.8, {x:1000});
+
+
+}
+
+animation_logos("logo_techno");*/
+
+              /*wow = new WOW(
+                      {
+                      boxClass:     'wow',
+                      animateClass: 'animated',
+                      offset:       0,
+                      mobile:       true,
+                      live:         true
+                    }
+                    )
+                    wow.init();*/
+
+window.onload = function(){
+
+}
