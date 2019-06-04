c7f1590f23f82f2c9ad65206e10801099d31739d
Helene Letac <helene.letac@coding-academy.fr>
2019-06-04 09:38:05 +0200

    portfolio sans les projets

--- projets_example/my_snapchat/my_snapchat_Onload_Im_Us.js
+++
@@ -1,9 +0,0 @@
-window.onload = function()
-{
-  image();
-  users();
-}
-
-/*Ce script permet de lier les pages image et users
-tout en évitant un conflit de .onload*/
-//3e scripts mentionné dans image html
